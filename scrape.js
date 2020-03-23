f1865ce03e9290ab45daa42718256b09981bbc19
Ben.Yoo <ben.yoo@perficient.com>
2020-03-22 22:33:00 -0400

    Able build message based on a passed in date from the user

--- scrape.js
+++ scrape.js
@@ -2,16 +2,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const moment = require('moment');
 
-let dateSelected = null || 'march 19 2020';
+const dateSelected = 'March 19, 2020' || moment().format('MMMM DD, YYYY');
+let dateTracker = dateSelected;
 let dateInString = moment(dateSelected).format('MMMM DD, YYYY');
 let continueAddingText = true;
-const finalMessage = {
-  [Date.parse(dateInString)]: {
-    text: '',
-  },
-};
+const finalMessage = {};
 const regexForDate = /(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}/;
-const test = async () => {
+const getWebContent = async () => {
   try {
     const result = await axios.get(
       'https://www.canada.ca/en/revenue-agency/campaigns/covid-19-update.html'
@@ -19,9 +16,12 @@ const test = async () => {
     const $ = cheerio.load(result.data);
     const testing = $('.mwspanel .section').map(async(index, element) => {
       try {
-        await buildMessage(element.children)
-        finalMessage[Date.parse(dateInString)].text =
-          finalMessage[Date.parse(dateInString)].text.replace(/^\s+|\s+$/g, '');
+        await buildMessage(element.children);
+        for (let key in finalMessage) {
+          console.log('Checking key', key);
+          finalMessage[key].text =
+            finalMessage[key].text.replace(/^\s+|\s+$/g, '');
+        }
         console.log('finalMessage', finalMessage);
       } catch (err) {
         console.log('Error each', err);
@@ -37,33 +37,52 @@ const buildMessage = async (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].data) {
       if (
-        Date.parse(arr[i].data)
-        && Date.parse(arr[i].data) >= Date.parse(dateInString)
+        arr[i].data.match(regexForDate)
+        && arr[i].data.match(regexForDate)[0] != undefined
+        && Date.parse(arr[i].data.match(regexForDate)[0])
+        && Date.parse(arr[i].data.match(regexForDate)[0]) > Date.parse(dateInString)
       ) {
-        dateSelected = arr[i].data;
-        console.log('Changed date', dateSelected);
-        const testMatch = arr[i].data.match(regexForDate);
-        console.log('test Match',testMatch);
+        console.log('Date greater', arr[i].data.match(regexForDate)[0]);
+        dateTracker = arr[i].data.match(regexForDate)[0];
         continue;
       } else if (
-        Date.parse(arr[i].data)
-        && Date.parse(arr[i].data) < Date.parse(dateInString)
+        arr[i].data.match(regexForDate)
+        && arr[i].data.match(regexForDate)[0] != undefined
+        && Date.parse(arr[i].data.match(regexForDate)[0])
+        && Date.parse(arr[i].data.match(regexForDate)[0]) === Date.parse(dateInString)
       ) {
-        const testMatch = arr[i].data.match(regexForDate);
-        console.log('test Match',testMatch);
+        console.log('Equal date', arr[i].data.match(regexForDate)[0]);
+        dateTracker = arr[i].data.match(regexForDate)[0];
+        continue;
+      } else if (
+        arr[i].data.match(regexForDate)
+        && arr[i].data.match(regexForDate)[0] != undefined
+        && Date.parse(arr[i].data.match(regexForDate)[0])
+        && Date.parse(arr[i].data.match(regexForDate)[0]) < Date.parse(dateInString)
+      ) {
+        console.log('Lower date', arr[i].data.match(regexForDate)[0]);
+        dateTracker = arr[i].data.match(regexForDate)[0];
         continueAddingText = false;
         return;
       }
-      console.log('date', Date.parse(dateInString))
+      console.log('Date trakcer', dateTracker);
+      console.log(finalMessage[`${Date.parse(dateTracker)}`])
+      console.log('Final text', JSON.stringify(finalMessage));
       continueAddingText
-        ? finalMessage[Date.parse(dateInString)].text += arr[i].data
+        ? (
+            finalMessage[`${Date.parse(dateTracker)}`]
+              ? finalMessage[`${Date.parse(dateTracker)}`].text += arr[i].data
+              : finalMessage[`${Date.parse(dateTracker)}`] = {
+                  text: arr[i].data,
+                }
+          )
         : i = arr.length;
       continue;
     }
 
     if (arr[i].name === 'a') {
-      const keys = Object.keys(finalMessage[Date.parse(dateInString)]).filter(x => x.includes('link'));
-      finalMessage[Date.parse(dateInString)][`link${keys.length + 1}`] =
+      const keys = Object.keys(finalMessage[Date.parse(dateTracker)]).filter(x => x.includes('link'));
+      finalMessage[Date.parse(dateTracker)][`link${keys.length + 1}`] =
         'https://www.canada.ca/en/revenue-agency/campaigns/covid-19-update.html'
         + arr[i].attribs.href;
     }
@@ -74,4 +93,4 @@ const buildMessage = async (arr) => {
   }
   return finalMessage;
 }
-test();
+getWebContent();
